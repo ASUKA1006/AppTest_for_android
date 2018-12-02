f3078aeb6ce78da12ab2f59e84ffc5b452971ccd
ASUKA1006 <asuka106@outlook.jp>
2018-12-02 15:15:40 +0900

    add scrollview

--- app/src/main/java/com/example/apptest/MainActivity.java
+++ app/src/main/java/com/example/apptest/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.apptest;
 
-        import android.graphics.Color;
-        import android.graphics.Typeface;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.TextView;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView text;
@@ -23,10 +23,11 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view){
+
                 if (flag){
                     text.setTextColor(Color.MAGENTA);
                     text.setTypeface(Typeface.DEFAULT_BOLD,35);
-                    text.setText("welcome to the world for programming!");
+                    text.setText("welcome to the world for programming!\n\n\n" + "you can do lots of things with programming.If you've never coded, that's ok. Just learn and enjoy it!" );
 
                     flag = false;
 
@@ -38,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
                     flag = true;
 
                 }
+
             }
 
 
