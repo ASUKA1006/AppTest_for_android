256a875f141c41411f601c83532fbd41be791ff2
ASUKA1006 <asuka106@outlook.jp>
2019-02-18 23:17:46 +0900

    created new activity

--- app/src/main/java/com/example/apptest/MainActivity.java
+++ app/src/main/java/com/example/apptest/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.apptest;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
     private TextView text;
@@ -26,12 +30,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view){
 
                 if (flag){
-                    text.setTextColor(Color.MAGENTA);
-                    text.setTypeface(Typeface.DEFAULT_BOLD,35);
-                    text.setText("welcome to the world for programming!\n\n\n" + "you can do lots of things with programming.If you've never coded, that's ok. Just learn and enjoy it!" );
-                    String mes = "To the next";
-                    Toast.makeText(MainActivity.this, mes, Toast.LENGTH_LONG).show();
-
+                    // use getApplication not to be out of memory
+                    Intent intent = new Intent(getApplication(), SubActivity.class);
+                    startActivity(intent);
                     flag = false;
 
                 }
@@ -43,10 +44,34 @@ public class MainActivity extends AppCompatActivity {
 
                 }
 
+
+
+
             }
-
-
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
 
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int selectedItem = item.getItemId();
+        int[] itemlist = {R.id.item1, R.id.item2, R.id.item3, R.id.item4};
+
+        for (int items : itemlist) {
+            if (items == selectedItem) {
+                Toast.makeText(MainActivity.this, "menu selected!", Toast.LENGTH_LONG).show();
+            }
+        }
+        if (selectedItem == R.id.item0){
+            Toast.makeText(MainActivity.this, "Still empty content", Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
+
